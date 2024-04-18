07dde8d67b2e5f6715d57cc3b60625653ae802ab
raissajamilec <145211026+raissajamilec@users.noreply.github.com>
2024-04-18 19:38:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// barra lateral
+const iconMenu = document.querySelector(".menu")
+const sidebar = document.querySelector(".sidebar")
+
+iconMenu.addEventListener("click", () => sidebar.classList.toggle("show-sidebar"))
